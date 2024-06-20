3f4c41212af59509e9607e4d0cec2f63b915e9f9
girl-on-a-string <frenchfries55@protonmail.com>
2024-06-20 16:23:34 -0500

    basic JS structure set up

---
+++ index.js
@@ -0,0 +1,23 @@
+function getComputerChoice () {
+    // return rock paper or scissors
+    // use math random
+
+}
+
+function getHumanChoice () {
+    // get one of the valid choices
+    //use prompt
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
