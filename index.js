126b1e1c11a7b3a7cfffa1e3f6cdbc4841063429
girl-on-a-string <frenchfries55@protonmail.com>
2024-06-20 18:14:11 -0500

    finished base code

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ function getComputerChoice () {
 
     switch (Math.floor(Math.random() * 3)) {
         case 0:
-            return "Rock";
+            return "rock";
             break;
         case 1:
-            return "Paper";
+            return "paper";
             break;
         case 2: 
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
@@ -19,14 +19,64 @@ function getHumanChoice () {
     // get one of the valid choices
     //use prompt
 
-    let ans = prompt('Choose "Rock", "Paper", or "Scissors"');
+    let ans = prompt('Choose "Rock", "Paper", or "Scissors"').toLowerCase;
+    return ans;
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound (humanChoice, computerChoice) {
+ 
+    if (computerChoice == "rock") {
 
+        if (humanChoice == "paper") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You win!`);
+
+        } else if (humanChoice == "scissors") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You lose!`);
+
+        } else {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. It's a tie!`);
+
+        }
+
+    } else if (computerChoice == "paper") {
+
+        if (humanChoice == "rock") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You lose!`);
+
+        } else if (humanChoice == "scissors") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You win!`);
+
+        } else {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. It's a tie!`);
+
+        }
+
+    } else {
+
+        if (humanChoice == "rock") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You win!`);
+
+        } else if (humanChoice == "paper") {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. You lose!`);
+
+        } else {
+
+            alert(`Computer chose ${computerChoice}, you chose ${humanChoice}. It's a tie!`);
+
+        }
+
+    }
 }
 
 const humanSelection = getHumanChoice();
