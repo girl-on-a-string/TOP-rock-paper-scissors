3fb7ed54ead1d795406b73a7fe4af347c3574b98
girl-on-a-string <frenchfries55@protonmail.com>
2024-06-20 17:43:26 -0500

    computer and human choice finishes

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ function getComputerChoice () {
     // return rock paper or scissors
     // use math random
 
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2: 
+            return "Scissors";
+            break;
+    }
 }
 
 function getHumanChoice () {
     // get one of the valid choices
     //use prompt
 
+    let ans = prompt('Choose "Rock", "Paper", or "Scissors"');
 }
 
 let humanScore = 0;
